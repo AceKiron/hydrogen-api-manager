e7593c8768356501abb11ab0e32695c233c4a434
Ace <39498818+AceKiron@users.noreply.github.com>
2022-07-26 13:20:51 +0000

    .

--- index.js
+++ index.js
@@ -4,10 +4,33 @@ require("dotenv").config();
 
 const chalk = require("chalk");
 
-function INFO(msg) { console.log(`[${chalk.gray(new Date())}] [${chalk.gray("INFO")}] ${msg}`); }
-function OK(msg) { console.log(`[${chalk.green(new Date())}] [${chalk.green("OK")}] ${msg}`); }
-function WARN(msg) { console.log(`[${chalk.yellow(new Date())}] [${chalk.yellow("WARN")}] ${msg}`); }
-function ERROR(msg) { console.log(`[${chalk.red(new Date())}] [${chalk.red("ERROR")}] ${msg}`); }
+let latestLogs = [];
+let maxLatestLogsLength = 32;
+
+function INFO(msg) {
+    console.log(`[${chalk.gray(new Date())}] [${chalk.gray("INFO")}] ${msg}`);
+    latestLogs.push(`[${new Date()}] [INFO] ${msg}`);
+    if (latestLogs.length == maxLatestLogsLength) latestLogs.shift();
+}
+
+function OK(msg) {
+    console.log(`[${chalk.green(new Date())}] [${chalk.green("OK")}] ${msg}`);
+    latestLogs.push(`[${new Date()}] [OK] ${msg}`);
+    if (latestLogs.length == maxLatestLogsLength) latestLogs.shift();
+}
+
+function WARN(msg) {
+    console.log(`[${chalk.yellow(new Date())}] [${chalk.yellow("WARN")}] ${msg}`);
+    latestLogs.push(`[${new Date()}] [WARN] ${msg}`);
+    if (latestLogs.length == maxLatestLogsLength) latestLogs.shift();
+}
+
+function ERROR(msg) {
+    console.log(`[${chalk.red(new Date())}] [${chalk.red("ERROR")}] ${msg}`);
+    latestLogs.push(`[${new Date()}] [ERROR] ${msg}`);
+    if (latestLogs.length == maxLatestLogsLength) latestLogs.shift();
+}
+
 function FATAL(msg, errorcode=-1) { console.log(chalk.bgRed(`[${new Date()}] [FATAL] ${msg}`)); process.exit(errorcode); }
 
 const express = require("express");
@@ -32,7 +55,10 @@ for (const modName of fs.readdirSync("./modules")) {
 
 app.get("/", (req, res) => {
     res.setHeader("Content-type", "text/plain");
-    res.send(`Hydrogen API Manager v${VERSION}`);
+    res.send(`Hydrogen API Manager v${VERSION}
+
+Logs:
+${latestLogs.join("\n")}`);
 });
 
 app.listen(process.env.PORT || 3157, () => {
--- modules/axolotl/index.js
+++ modules/axolotl/index.js
@@ -3,32 +3,38 @@ const fs = require("fs");
 const axios = require("axios");
 
 let VIDEO_ID;
+let VIDEO_DATA;
 
 let cooldown = Date.now();
 
-async function updateVideoId(WARN) {
+async function updateVideoId(INFO, WARN) {
     const req = await axios({
         method: "GET",
         url: `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCrhYiGXMwsfXB3QTCHmFQiQ&eventType=live&type=video&key=${process.env.GOOGLE_API_KEY}`
     });
 
-    if (req.data.items.length == 0) WARN("Gary the Axolotl isn't live right now");
-    else VIDEO_ID = req.data.items[0].id.videoId;
+    let items = req.data.items.filter((item) => item.snippet.title === "Axolotl Live 24/7 With Gary The Axolotl");
+
+    if (items.length == 0) WARN("Gary the Axolotl isn't live right now");
+    else {
+        VIDEO_DATA = items[0];
+        INFO(JSON.stringify(VIDEO_DATA, null, 2));
+    }
 }
 
 module.exports = async ({ router, INFO, WARN }) => {
-    await updateVideoId(WARN);
+    await updateVideoId(INFO, WARN);
 
     router.get("/garytheaxolotl", async (req, res) => {
-        if (VIDEO_ID === undefined) {
+        if (VIDEO_DATA === undefined) {
             res.sendStatus(503);
-            return await updateVideoId(WARN);
+            return await updateVideoId(INFO, WARN);
         }
         
         if (Date.now() >= cooldown + 10000) {
             // Fetch new image every 10 seconds
             cooldown = Date.now();
-            cp.exec(`ffmpeg -i "$(yt-dlp -g ${VIDEO_ID} | head -n 1)" -vframes 1 last.jpg -y -v quiet`);
+            cp.exec(`ffmpeg -i "$(yt-dlp -g ${VIDEO_DATA.id.videoId} | head -n 1)" -vframes 1 last.jpg -y -v quiet`);
             INFO("Updating GaryTheAxolotl image");
         }
         
@@ -37,11 +43,15 @@ module.exports = async ({ router, INFO, WARN }) => {
     });
 
     router.get("/garytheaxolotl/meta", async (req, res) => {
-        if (VIDEO_ID === undefined) {
+        if (VIDEO_DATA === undefined) {
             res.sendStatus(503);
-            return await updateVideoId(WARN);
+            return await updateVideoId(INFO, WARN);
         }
 
-        res.send({ videoId: VIDEO_ID, channelId: "UCrhYiGXMwsfXB3QTCHmFQiQ" });
+        res.send({
+            liveVideoId: VIDEO_DATA.id.videoId,
+            liveSince: new Date(VIDEO_DATA.snippet.publishTime).toUTCString(),
+            channelId: "UCrhYiGXMwsfXB3QTCHmFQiQ"
+        });
     });
 }
